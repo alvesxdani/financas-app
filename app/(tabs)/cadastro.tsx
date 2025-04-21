cb8391fb61ce6db938419769ebf5f0a18c697219
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-21 15:15:52 -0300

    feat: initial component

---
+++ app/(tabs)/cadastro.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Cadastro = () => {
+  return (
+    <SafeAreaView>
+      <Text>Cadastro</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Cadastro
