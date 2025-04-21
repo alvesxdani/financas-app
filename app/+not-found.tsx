0a8969c3bebe12751351f1c71fd15600a08e0547
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-21 15:15:12 -0300

    refactor: adjusting the structure of the page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,16 @@
-import { Stack } from 'expo-router'
+import { Link } from 'expo-router'
 import { StyleSheet, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <SafeAreaView>
-        <Text>Ops! Página não encontrada.</Text>
+      <SafeAreaView className="flex-1 items-center justify-center p-5">
+        <Text className="font-semibold text-2xl text-center">Ops!</Text>
+        <Text>Essa página não existe.</Text>
+        <Text>
+          <Link href={'/'}>Voltar para Home</Link>
+        </Text>
       </SafeAreaView>
     </>
   )
