2979705444ca2ada7c51cdef54850b43d84302d4
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-23 18:57:59 -0300

    feat: add category select field

--- app/(tabs)/cadastro.tsx
+++ app/(tabs)/cadastro.tsx
@@ -8,7 +8,9 @@ import {
 import { Input, InputField, InputSlot } from '@/components/ui/input'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import DateTimePicker from '@react-native-community/datetimepicker'
+import { Picker } from '@react-native-picker/picker'
 import { useState } from 'react'
+import { View } from 'react-native'
 import CurrencyInput from 'react-native-currency-input'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -37,6 +39,16 @@ const Cadastro = () => {
   const [form, setForm] = useState<CadastroT>(initialStateForm)
   const [error, setError] = useState(initialStateErrors)
   const [show, setShow] = useState(false)
+
+  const categoriesOptions = [
+    { label: 'Renda', value: 'renda' },
+    { label: 'Educação', value: 'educacao' },
+    { label: 'Alimentação', value: 'alimentacao' },
+    { label: 'Contas', value: 'contas' },
+    { label: 'Saúde', value: 'saude' },
+    { label: 'Viagem', value: 'viagem' },
+    { label: 'Outros', value: 'outros' },
+  ]
 
   function handleValidate(field: string, value: any) {
     setError((prev) => ({ ...prev, [field]: false }))
@@ -131,6 +143,30 @@ const Cadastro = () => {
             <MaterialIcons name="calendar-today" className="mr-3" size={18} />
           </InputSlot>
         </Input>
+        <FormControlError>
+          <MaterialIcons name="error" size={24} color="red" />
+          <FormControlErrorText>Campo obrigatório.</FormControlErrorText>
+        </FormControlError>
+      </FormControl>
+
+      <FormControl isRequired size="sm" isInvalid={error.category}>
+        <FormControlLabel>
+          <FormControlLabelText className="font-semibold">
+            Categoria
+          </FormControlLabelText>
+        </FormControlLabel>
+        <View className="border border-neutral-300">
+          <Picker
+            mode="dropdown"
+            className="border"
+            selectedValue={form.category}
+            onValueChange={(value) => setForm({ ...form, category: value })}
+          >
+            {categoriesOptions.map(({ label, value }, index) => (
+              <Picker.Item {...{ label, value }} key={index} />
+            ))}
+          </Picker>
+        </View>
         <FormControlError>
           <MaterialIcons name="error" size={24} color="red" />
           <FormControlErrorText>Campo obrigatório.</FormControlErrorText>
