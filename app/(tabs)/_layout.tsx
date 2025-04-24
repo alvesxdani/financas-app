d7589af015d09fce12ac69b060fe9e3bfcd74cd8
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-23 23:53:23 -0300

    feat: add context for transactions

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import TransactionsContextProvider from '@/contexts/transactions.context'
 import { COLORS } from '@/utils/colors.utils'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { Tabs } from 'expo-router'
@@ -5,49 +6,51 @@ import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: COLORS.main,
-        headerStyle: {
-          backgroundColor: COLORS.main,
-        },
-        headerTintColor: COLORS.white,
-        headerTitleAlign: 'center',
-        tabBarInactiveTintColor: COLORS.inactive,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="attach-money" size={24} color={color} />
-          ),
-          tabBarLabel: 'Transações',
-          headerTitle: 'Transações',
+    <TransactionsContextProvider>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: COLORS.main,
+          headerStyle: {
+            backgroundColor: COLORS.main,
+          },
+          headerTintColor: COLORS.white,
+          headerTitleAlign: 'center',
+          tabBarInactiveTintColor: COLORS.inactive,
         }}
-      />
-      <Tabs.Screen
-        name="cadastro"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <View className="flex items-center justify-center !w-16 !h-16 rounded-full bg-main">
-              <MaterialIcons name="add" size={28} color={COLORS.white} />
-            </View>
-          ),
-          tabBarLabelStyle: { display: 'none' },
-          headerTitle: 'Adicionar transação',
-        }}
-      />
-      <Tabs.Screen
-        name="resumo"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="pie-chart" size={24} color={color} />
-          ),
-          tabBarLabel: 'Resumo',
-          headerTitle: 'Resumo',
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="attach-money" size={24} color={color} />
+            ),
+            tabBarLabel: 'Transações',
+            headerTitle: 'Transações',
+          }}
+        />
+        <Tabs.Screen
+          name="cadastro"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <View className="flex items-center justify-center !w-16 !h-16 rounded-full bg-main">
+                <MaterialIcons name="add" size={28} color={COLORS.white} />
+              </View>
+            ),
+            tabBarLabelStyle: { display: 'none' },
+            headerTitle: 'Adicionar transação',
+          }}
+        />
+        <Tabs.Screen
+          name="resumo"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="pie-chart" size={24} color={color} />
+            ),
+            tabBarLabel: 'Resumo',
+            headerTitle: 'Resumo',
+          }}
+        />
+      </Tabs>
+    </TransactionsContextProvider>
   )
 }
---
+++ contexts/transactions.context.tsx
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createContext, useEffect, useState } from 'react'
+
+type TransactionT = {
+  title: string
+  value: number | null
+  date: string
+  category: string
+}
+
+export const TransactionsContext = createContext<Array<any>>([])
+
+export default function TransactionsContextProvider({ children }: any) {
+  const [transactions, setTransactions] = useState<TransactionT[]>([])
+
+  useEffect(() => {
+    async function loadTransactions() {
+      // AsyncStorage.clear()
+      const data = await AsyncStorage.getItem('data')
+      if (data) {
+        setTransactions(JSON.parse(data))
+      }
+    }
+
+    loadTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={[transactions, setTransactions]}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
