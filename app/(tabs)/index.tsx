38b4857189776fa952449d7aeaeb7713093f83b2
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-23 23:02:11 -0300

    refactor: adjusting list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,110 +2,125 @@ import { Box } from '@/components/ui/box'
 import { COLORS } from '@/utils/colors.utils'
 import { formatMoney } from '@/utils/index.utils'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
 import { FlatList, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+type TransacaoT = {
+  title: string
+  value: number | null
+  date: string
+  category: string
+}
+
 const Home = () => {
-  const transacoes = [
-    {
-      data: '03/03/2025',
-      descricao: 'Salário',
-      valor: 3000.0,
-      tipo: 'entrada',
-      categoria: 'salario',
-    },
-    {
-      data: '05/03/2025',
-      descricao: 'Faculdade',
-      valor: 1200.0,
-      tipo: 'saida',
-      icon: 'menu-book',
-      categoria: 'educacao',
-    },
-    {
-      data: '08/03/2025',
-      descricao: 'Marmita',
-      valor: 25.0,
-      tipo: 'saida',
-      categoria: 'alimentacao',
-    },
-    {
-      data: '12/03/2025',
-      descricao: 'Prestação Paris',
-      valor: 500.0,
-      tipo: 'saida',
-      categoria: 'viagem',
-    },
-    {
-      data: '15/03/2025',
-      descricao: 'Conta de Luz',
-      valor: 310.0,
-      tipo: 'saida',
-      categoria: 'contas',
-    },
-    {
-      data: '18/03/2025',
-      descricao: 'Sanduíche',
-      valor: 52.0,
-      tipo: 'saida',
-      categoria: 'alimentacao',
-    },
-  ]
+  const [transacoes, setTransacoes] = useState<TransacaoT[]>([])
 
-  const SET_ICON_CLASS_COLOR = (category: string) => {
+  useEffect(() => {
+    async function loadTransacoes() {
+      // AsyncStorage.clear()
+      const data = await AsyncStorage.getItem('data')
+      if (data) {
+        setTransacoes(JSON.parse(data))
+      }
+      console.log('Transações:', data)
+      console.log('Quantidade:', data?.length)
+    }
+
+    loadTransacoes()
+  }, [])
+
+  const SET_ICON = (
+    category: string,
+  ): {
+    color: string
+    icon:
+      | 'attach-money'
+      | 'school'
+      | 'fastfood'
+      | 'credit-card'
+      | 'flight'
+      | 'help'
+      | 'local-hospital'
+    transitionColor?: string
+  } => {
     switch (category) {
-      case 'salario':
-        return '!bg-purple-400'
+      case 'renda':
+        return {
+          color: 'bg-purple-400',
+          icon: 'attach-money',
+          transitionColor: 'text-green-500',
+        }
       case 'educacao':
-        return '!bg-indigo-400'
+        return {
+          color: 'bg-indigo-400',
+          icon: 'school',
+          transitionColor: 'text-red-500',
+        }
       case 'alimentacao':
-        return 'bg-rose-300'
+        return {
+          color: 'bg-rose-300',
+          icon: 'fastfood',
+          transitionColor: 'text-red-500',
+        }
       case 'contas':
-        return 'bg-amber-300'
+        return {
+          color: 'bg-amber-300',
+          icon: 'credit-card',
+          transitionColor: 'text-red-500',
+        }
       case 'viagem':
-        return 'bg-blue-400'
+        return {
+          color: 'bg-blue-400',
+          icon: 'flight',
+          transitionColor: 'text-red-500',
+        }
+      case 'saude':
+        return {
+          color: 'bg-green-400',
+          icon: 'local-hospital',
+          transitionColor: 'text-red-500',
+        }
       default:
-        return 'bg-gray-400'
+        return {
+          color: 'bg-gray-400',
+          icon: 'help',
+        }
     }
-  }
-
-  const SET_ICON = (category: string) => {
-    const icons: Record<
-      string,
-      'work' | 'school' | 'fastfood' | 'credit-card' | 'flight'
-    > = {
-      salario: 'work',
-      educacao: 'school',
-      alimentacao: 'fastfood',
-      contas: 'credit-card',
-      viagem: 'flight',
-    }
-
-    return icons[category]
   }
 
   return (
     <SafeAreaView className="px-2">
-      <FlatList
-        data={transacoes}
-        renderItem={({ item }) => (
-          <Box className="flex-row items-center justify-between gap-2 px-2 py-3 mb-2 border-b border-b-gray-200">
-            <MaterialIcons
-              name={SET_ICON(item.categoria)}
-              size={36}
-              color={COLORS.white}
-              className={`${SET_ICON_CLASS_COLOR(
-                item.categoria,
-              )} p-2 rounded-full`}
-            />
-            <Box className="grid gap-1 flex-1">
-              <Text className="text-2xs text-neutral-400">{item.data}</Text>
-              <Text className="font-semibold">{item.descricao}</Text>
+      {!transacoes.length && (
+        <Text className="text-center text-neutral-400">
+          Nenhuma transação encontrada
+        </Text>
+      )}
+      {transacoes.length > 0 && (
+        <FlatList
+          data={transacoes}
+          renderItem={({ item }) => (
+            <Box className="flex-row items-center justify-between gap-2 px-2 py-3 mb-2 border-b border-b-gray-200">
+              <MaterialIcons
+                name={SET_ICON(item.category).icon}
+                size={36}
+                color={COLORS.white}
+                className={`${SET_ICON(item.category).color} p-2 rounded-full`}
+              />
+              <Box className="grid gap-1 flex-1">
+                <Text className="text-2xs text-neutral-400">
+                  {new Date(item.date).toLocaleDateString('pt-BR')}
+                </Text>
+                <Text className="font-semibold">{item.title}</Text>
+              </Box>
+              <Text className={SET_ICON(item.category).transitionColor}>
+                {formatMoney(item.value as number)}
+              </Text>
             </Box>
-            <Text>{formatMoney(item.valor)}</Text>
-          </Box>
-        )}
-      />
+          )}
+        />
+      )}
     </SafeAreaView>
   )
 }
