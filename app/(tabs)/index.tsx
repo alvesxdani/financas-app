149f713e68b8f58ef7ba8b4542f4cce83984c6e4
Daniela Alves <dani.alvesm22@gmail.com>
2025-04-24 00:18:41 -0300

    refactor: removing data state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,13 @@
 import { Box } from '@/components/ui/box'
+import { TransactionsContext } from '@/contexts/transactions.context'
 import { COLORS } from '@/utils/colors.utils'
 import { formatMoney } from '@/utils/index.utils'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useEffect, useState } from 'react'
-import { FlatList, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-
-type TransacaoT = {
-  title: string
-  value: number | null
-  date: string
-  category: string
-}
+import { useContext } from 'react'
+import { FlatList, Text, View } from 'react-native'
 
 const Home = () => {
-  const [transacoes, setTransacoes] = useState<TransacaoT[]>([])
-
-  useEffect(() => {
-    async function loadTransacoes() {
-      // AsyncStorage.clear()
-      const data = await AsyncStorage.getItem('data')
-      if (data) {
-        setTransacoes(JSON.parse(data))
-      }
-      console.log('Transações:', data)
-      console.log('Quantidade:', data?.length)
-    }
-
-    loadTransacoes()
-  }, [])
+  const [transactions] = useContext(TransactionsContext)
 
   const SET_ICON = (
     category: string,
@@ -43,44 +21,44 @@ const Home = () => {
       | 'flight'
       | 'help'
       | 'local-hospital'
-    transitionColor?: string
+    transactionColor?: string
   } => {
     switch (category) {
       case 'renda':
         return {
           color: 'bg-purple-400',
           icon: 'attach-money',
-          transitionColor: 'text-green-500',
+          transactionColor: 'text-green-500',
         }
       case 'educacao':
         return {
           color: 'bg-indigo-400',
           icon: 'school',
-          transitionColor: 'text-red-500',
+          transactionColor: 'text-red-500',
         }
       case 'alimentacao':
         return {
           color: 'bg-rose-300',
           icon: 'fastfood',
-          transitionColor: 'text-red-500',
+          transactionColor: 'text-red-500',
         }
       case 'contas':
         return {
           color: 'bg-amber-300',
           icon: 'credit-card',
-          transitionColor: 'text-red-500',
+          transactionColor: 'text-red-500',
         }
       case 'viagem':
         return {
           color: 'bg-blue-400',
           icon: 'flight',
-          transitionColor: 'text-red-500',
+          transactionColor: 'text-red-500',
         }
       case 'saude':
         return {
           color: 'bg-green-400',
           icon: 'local-hospital',
-          transitionColor: 'text-red-500',
+          transactionColor: 'text-red-500',
         }
       default:
         return {
@@ -91,15 +69,15 @@ const Home = () => {
   }
 
   return (
-    <SafeAreaView className="px-2">
-      {!transacoes.length && (
+    <View className="px-2">
+      {!transactions.length && (
         <Text className="text-center text-neutral-400">
           Nenhuma transação encontrada
         </Text>
       )}
-      {transacoes.length > 0 && (
+      {transactions.length > 0 && (
         <FlatList
-          data={transacoes}
+          data={transactions}
           renderItem={({ item }) => (
             <Box className="flex-row items-center justify-between gap-2 px-2 py-3 mb-2 border-b border-b-gray-200">
               <MaterialIcons
@@ -114,14 +92,14 @@ const Home = () => {
                 </Text>
                 <Text className="font-semibold">{item.title}</Text>
               </Box>
-              <Text className={SET_ICON(item.category).transitionColor}>
+              <Text className={SET_ICON(item.category).transactionColor}>
                 {formatMoney(item.value as number)}
               </Text>
             </Box>
           )}
         />
       )}
-    </SafeAreaView>
+    </View>
   )
 }
 
